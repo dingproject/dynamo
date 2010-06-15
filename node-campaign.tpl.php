113eafe6a4b2b2d3fecdfd7099cfdd264dd2acb2
Kasper Garnæs <kasper@reload.dk>
2010-06-15 13:04:04 +0200

    Use url instead of display-url when generating campaign links. Fixes #1271.

--- node-campaign.tpl.php
+++ node-campaign.tpl.php
@@ -1,13 +1,13 @@
 <?php if ($node->campaign_type == "image-only"): ?>
 	<div class="campaign-image campaign-type-<?php print $node->campaign_type;  ?> clearfix">
-		<?php print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE)); ?>
+		<?php print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['url'], $options= array('html'=>TRUE)); ?>
 	</div>
 <?php else: ?>
 	<div class="campaign-text clearfix">
 		<div class="campaign-inner">
 			<div class="campaign-type-<?php print $node->campaign_type;?>">
 				<div class="campaign-theme campaign-theme-<?php print $node->campaign_theme; ?>">
-					<?php print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['display_url'], $options= array('html'=>TRUE)); ?>
+					<?php print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['url'], $options= array('html'=>TRUE)); ?>
 				</div>
 			</div>
 		</div>
