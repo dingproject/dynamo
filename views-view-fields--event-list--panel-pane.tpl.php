2be0e429bc57ca7705a4f405a2288781a3fb567f
Mikkel Hoegh <mikkel@hoegh.org>
2010-08-07 22:02:34 +0200

    Fixed the date and price output on the events page

--- views-view-fields--event-list--panel-pane.tpl.php
+++ views-view-fields--event-list--panel-pane.tpl.php
@@ -1,11 +1,12 @@
- <!-- views-view-fields  event-list  page.tpl.php  (panel pane)-->
 <?php 
-  // Convert the date value to timestamp
-  $date = theme('event_information',
-                strtotime($fields['field_datetime_value']->raw . 'Z'),
-                strtotime($fields['field_datetime_value']->raw . 'Z'),
-                $fields['field_entry_price_value']->raw);
-  //$date = strtotime($fields['field_datetime_value']->raw . 'Z');
+// $Id$
+
+/**
+ * Template to render a row from the event_list view.
+ */
+
+$start = date_make_date($fields['field_datetime_value']->raw);
+$price = ($fields['field_entry_price_value']->raw == 0) ? t('Free') : $fields['field_entry_price_value']->raw;
 ?>
 <div class="node-teaser clearfix">
 
@@ -19,17 +20,17 @@
 		<h2><?php print $fields['title']->content; ?></h2>
 
 		<div class="meta">
-		  <?php print $date['date']; ?>
+      <?php print $start->format('j. F Y'); ?>
 		  <span class="library-tag"><?php print $fields['field_library_ref_nid']->content; ?></span>, 
-		  <span class="price"><?php print $date['price']; ?></span>
+		  <span class="price"><?php print $price; ?></span>
 		</div>    
     
 		<p> 
 		 <?php 
-	 			if($fields['field_teaser_value']->content){
+	 			if ($fields['field_teaser_value']->content) {
 					print $fields['field_teaser_value']->content; 
-
-				}else{
+        }
+        else {
 					print $fields['body']->content;       
 	 			}
  			?>
