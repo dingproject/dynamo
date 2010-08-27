8ab96cf1f9f0bce493fff0af072f9ca63d226a47
Mikkel Hoegh <mikkel@hoegh.org>
2010-08-27 18:31:39 +0200

    Code style and formatting.

--- node-event.tpl.php
+++ node-event.tpl.php
@@ -14,24 +14,25 @@ if ($page == 0): ?>
 
   <div class="content">
 
-  	<?php if ($node->title): ?>
-      <h3><?php print l($node->title, 'node/'.$node->nid); ?></h3>
-  	<?php endif; ?>
+    <?php if ($node->title): ?>
+      <h3><?php print l($node->title, 'node/' . $node->nid); ?></h3>
+    <?php endif; ?>
 
-  	<div class="meta">
+    <div class="meta">
       <?php print $field_datetime_rendered ?>
-      <?php print $field_library_ref_rendered ?>          
+      <?php print $field_library_ref_rendered ?>
       <?php print $field_entry_price_rendered ?>
-  	</div>
-	
-		<?php 
-			//field_teaser
-				if($node->field_teaser[0]['value']){
-					print $node->field_teaser[0]['value'];
-				}else{
-					print strip_tags($node->content['body']['#value']);	
-				}
-			?>
+    </div>
+
+    <?php
+      //field_teaser
+      if ($node->field_teaser[0]['value']) {
+        print $node->field_teaser[0]['value'];
+      }
+      else {
+        print strip_tags($node->content['body']['#value']);
+      }
+    ?>
 
 
   <?php // adding warning for event that has already occurred
@@ -39,7 +40,7 @@ if ($page == 0): ?>
     <div class="alert"><?php print t('NB! This event occurred in the past.'); ?></div>
   <?php endif; ?>
   </div>
-    
+
   </div>
 
 </div>
@@ -48,46 +49,46 @@ if ($page == 0): ?>
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix">
 
-	<?php if($node->title){	?>	
-	  <h2><?php print $title;?></h2>
-	<?php } ?>
+  <?php if($node->title){ ?>
+    <h2><?php print $title;?></h2>
+  <?php } ?>
 
-	<?php
-		// adding warning for event that has already occurred
-		print $alertbox;
-	?>
+  <?php
+    // adding warning for event that has already occurred
+    print $alertbox;
+  ?>
 
-	<div class="content">
-		<div class="event-info">
+  <div class="content">
+    <div class="event-info">
       <span class="event-date"><?php print $event_info['date']; ?></span>
       <?php if ($event_info['time']): ?>
         <span class="time"><?php print $event_info['time']; ?></span>
       <?php endif; ?>
-			<span class="event-price"><?php print $event_info['price']; ?>  </span>
-		</div>
+      <span class="event-price"><?php print $event_info['price']; ?>  </span>
+    </div>
 
-		<?php print $content ?>
-	</div>
-		
-	<div class="meta">
-		<span class="time">
-			<?php print t('This event was created'); ?>
-			<?php print format_date($node->created, 'custom', "j. F Y") ?> 
-		</span>	
-		<span class="author">
-			<?php print t('by'); ?> <?php print theme('username', $node); ?>
-		</span>	
+    <?php print $content ?>
+  </div>
 
-		<?php if (count($taxonomy)){ ?>
-		  <div class="taxonomy">
-	   	  <?php print t("Tags:") . " " .  $terms ?> 
-		  </div>  
-		<?php } ?>
-	</div>		
-		
-	<?php if ($links){ ?>
+  <div class="meta">
+    <span class="time">
+      <?php print t('This event was created'); ?>
+      <?php print format_date($node->created, 'custom', "j. F Y") ?>
+    </span>
+    <span class="author">
+      <?php print t('by'); ?> <?php print theme('username', $node); ?>
+    </span>
+
+    <?php if (count($taxonomy)){ ?>
+      <div class="taxonomy">
+        <?php print t("Tags:") . " " .  $terms ?>
+      </div>
+    <?php } ?>
+  </div>
+
+  <?php if ($links){ ?>
     <?php  print $links; ?>
-	<?php } ?>
+  <?php } ?>
 </div>
 <?php endif; ?>
 
