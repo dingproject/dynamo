51ab4393cba504176f74cdfe325aa5c8e7d37d85
Thomas Fini Hansen <xen@xen.dk>
2010-12-02 12:50:45 +0100

    Fixed typo and tabs.

--- panels-threecol-left-stacked-right.tpl.php
+++ panels-threecol-left-stacked-right.tpl.php
@@ -1,33 +1,33 @@
 <!-- panels-threecol-left-stacked-right.tpl.php -->
 <div class="panel-threecol-left-stacked-right clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-		<div class="content-left">
+    <div class="content-left">
 
-				<div class="content-top">
-	      	<?php print $content['top']; ?>
-				</div>
+        <div class="content-top">
+          <?php print $content['top']; ?>
+        </div>
 
-				<div class="content-middle">
+        <div class="content-middle">
 
-					<div class="panel-left">
-				      <?php print $content['left']; ?>      		
-					</div>
+          <div class="panel-left">
+              <?php print $content['left']; ?>
+          </div>
 
-					<div class="panel-middle">
-				      <?php print $content['middle']; ?>        
-					</div>
+          <div class="panel-middle">
+              <?php print $content['middle']; ?>
+          </div>
 
-				</div>
+        </div>
 
-				<div class="panel-bottomt">
-	      	<?php print $content['bottom']; ?>        
-				</div>
+        <div class="panel-bottom">
+          <?php print $content['bottom']; ?>
+        </div>
 
-		</div>
+    </div>
 
-		<div class="content-right">
-	      <?php print $content['right']; ?>
-		</div>
+    <div class="content-right">
+        <?php print $content['right']; ?>
+    </div>
 
 
 </div>
--- panels-threecol-left-stacked.tpl.php
+++ panels-threecol-left-stacked.tpl.php
@@ -1,34 +1,34 @@
 <!--panels-threecol-left-stacked.tpl.php-->
 <div class="panel-threecol-left-stacked clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
 
-	<div class="content-left">
+  <div class="content-left">
 
-			<div class="content-top">
-      	<?php print $content['top']; ?>
-			</div>
+      <div class="content-top">
+        <?php print $content['top']; ?>
+      </div>
 
-			<div class="content-middle">
+      <div class="content-middle">
 
-				<div class="panel-left">
-			      <?php print $content['left']; ?>      		
-				</div>
+        <div class="panel-left">
+            <?php print $content['left']; ?>
+        </div>
 
-				<div class="panel-middle">
-			      <?php print $content['middle']; ?>        
-				</div>
+        <div class="panel-middle">
+            <?php print $content['middle']; ?>
+        </div>
 
-			</div>
+      </div>
 
-			<div class="panel-bottomt">
-      	<?php print $content['bottom']; ?>        
-			</div>
+      <div class="panel-bottom">
+        <?php print $content['bottom']; ?>
+      </div>
 
-	</div>
+  </div>
 
-	<div class="content-right">
+  <div class="content-right">
       <?php print $content['right']; ?>
-	</div>
-	
+  </div>
+
 </div>
 
 <!--/panels-threecol-left-stacked.tpl.php-->
