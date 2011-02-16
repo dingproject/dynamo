4d10994ac3d78d77fec6ee5d29cf6c0898941f87
Mikkel Hoegh <mikkel@hoegh.org>
2011-02-16 15:12:23 +0100

    Clearer comments.

--- js/dynamo.js
+++ js/dynamo.js
@@ -3,20 +3,20 @@
  * JavaScript bindings and behaviors for the Dynamo theme for Ding.
  */
 
-$(document).ready(function() {
-
-  //hide messages
+// To do when the document is ready.
+jQuery(function($) {
+  // Button for hiding messages.
   $("#messages-hide a").click(function (event) {
-      $('#drupal-messages').slideUp();
+    $('#drupal-messages').slideUp();
   });
 
-  //label
+  // In-field labels for the login form.
   $("#pageheader label").inFieldLabels({
     fadeOpacity:"0.2",
     fadeDuration:"100"
   });
 
-  //carousel
+  // jCarousel for the front page.
   if ($('#event-similar')) {
     $('#event-similar').jcarousel({
        vertical: false, //
