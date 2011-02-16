596b92ea542000225335d331fa302556545a60eb
Mikkel Hoegh <mikkel@hoegh.org>
2011-02-16 15:08:29 +0100

    Whitespace cleanup and initial comment.

--- js/dynamo.js
+++ js/dynamo.js
@@ -1,38 +1,30 @@
+/**
+ * @file
+ * JavaScript bindings and behaviors for the Dynamo theme for Ding.
+ */
+
 $(document).ready(function() {
 
+  //hide messages
+  $("#messages-hide a").click(function (event) {
+      $('#drupal-messages').slideUp();
+  });
 
-	//hide messages
- 	$("#messages-hide a").click(function(event){
-			$('#drupal-messages').slideUp();
-	});
+  //label
+  $("#pageheader label").inFieldLabels({
+    fadeOpacity:"0.2",
+    fadeDuration:"100"
+  });
 
-
-	//password iphone thingie
-//		$('input:password').dPassword({
-		// $('#pageheader #edit-name').dPassword({
-		// 		duration: 2000,
-		// 		prefix: 'my_'
-		// });
-
-	//label
-	  $("#pageheader label").inFieldLabels({
-			fadeOpacity:"0.2",
-			fadeDuration:"100"			
-		});
-
-
-//carousel
-
-	if($('#event-similar')){
+  //carousel
+  if ($('#event-similar')) {
     $('#event-similar').jcarousel({
        vertical: false, //
        scroll: 1, //amount of items to scroll by
        animation: "slow", // slow - fast
        auto: "0", //autoscroll in secunds
        wrap: "last"
-     });
-	}
-
+    });
+  }
 });
-
 
