c083c3ce597a75c4ca087b45837861c2aa6d1f82
Hasse R. Hansen <hasse@ramlev.dk>
2011-03-31 14:19:11 +0200

    Use dynamo date function to format correct

--- views-view-fields--event-list--panel-pane.tpl.php
+++ views-view-fields--event-list--panel-pane.tpl.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // $Id$
 
 /**
@@ -17,27 +17,27 @@ $price = ($fields['field_entry_price_value']->raw < 1) ? t('Free') : intval($fie
   </div>
 
   <div class="info">
-  
+
 		<h2><?php print $fields['title']->content; ?></h2>
 
 		<div class="meta">
-      <?php print date_format_date($start, 'short'); ?>
-		  <span class="library-tag"><?php print $fields['field_library_ref_nid']->content; ?></span>, 
+      <?php print dynamo_datef($start, 'j. F Y'); ?>
+		  <span class="library-tag"><?php print $fields['field_library_ref_nid']->content; ?></span>,
 		  <span class="price"><?php print $price; ?></span>
-		</div>    
-    
-		<p> 
-		 <?php 
+		</div>
+
+		<p>
+		 <?php
 	 			if ($fields['field_teaser_value']->content) {
-					print $fields['field_teaser_value']->content; 
+					print $fields['field_teaser_value']->content;
         }
         else {
-					print $fields['body']->content;       
+					print $fields['body']->content;
 	 			}
  			?>
-		</p> 
-		
-    <?php print $fields['edit_node']->content; ?>  
+		</p>
+
+    <?php print $fields['edit_node']->content; ?>
   </div>
 </div>
 
