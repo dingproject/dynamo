b6e535d8cfa7e73fa100cbc2934d4070f7c6d62c
troelslenda <troels+git@reload.dk>
2011-04-20 11:28:29 +0200

    Queries in the campaign URL's are no longer stripped away.

--- node-campaign.tpl.php
+++ node-campaign.tpl.php
@@ -8,14 +8,14 @@
 
 if ($node->campaign_type == "image-only"): ?>
 	<div class="campaign-image campaign-type-<?php print $node->campaign_type;  ?> clearfix">
-		<?php print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['url'], $options= array('html'=>TRUE)); ?>
+		<?php print l($node->field_campaign_image['0']['view'], $node->field_campaign_link['0']['url'], $options= array('html'=>TRUE,'query' => $node->field_campaign_link['0']['query'])); ?>
 	</div>
 <?php else: ?>
 	<div class="campaign-text clearfix">
 		<div class="campaign-inner">
 			<div class="campaign-type-<?php print $node->campaign_type;?>">
 				<div class="campaign-theme campaign-theme-<?php print $node->campaign_theme; ?>">
-					<?php print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['url'], $options= array('html'=>TRUE)); ?>
+					<?php print l(filter_xss($node->content['body']['#value']),  $node->field_campaign_link['0']['url'], $options= array('html'=>TRUE,'query' => $node->field_campaign_link['0']['query'])); ?>
 				</div>
 			</div>
 		</div>
